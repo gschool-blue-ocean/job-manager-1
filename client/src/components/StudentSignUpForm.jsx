348784f3f0fdd53b92495a7090f68f51f79401d0
Will-Swinson <william.swinson01@gmail.com>
2023-07-15 21:51:12 -0400

    added student signup form submit feat.

--- client/src/components/StudentSignUpForm.jsx
+++ client/src/components/StudentSignUpForm.jsx
@@ -23,79 +23,88 @@ export default function StudentSignUpForm() {
   console.log("studentForm", studentForm);
 
   return (
-    <div className="bg-gray-700 relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-gray-700 rounded-md shadow-xl lg:max-w-xl">
-        <h1 className="text-3xl font-semibold text-center text-white underline">
-          Student Sign up
-        </h1>
-<<<<<<< HEAD
-        <form className="mt-6">
-=======
-        <form onSubmit={handleSubmitStudentForm} className="mt-6">
-          <div className="mb-2">
-            <label
-              htmlFor="name"
-              className="block text-sm font-semibold text-white"
-            >
-              Name
-            </label>
-            <input
-              type="name"
-              className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              onChange={(e) =>
-                setStudentForm({ ...studentForm, name: e.target.value })
-              }
-              value={studentForm.name}
-            />
-          </div>
->>>>>>> 423d3d3 (routes and auto search fix)
-          <div className="mb-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-white"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              className="block w-full px-4 py-2 mt-2 text-white bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              onChange={(e) =>
-                setStudentForm({ ...studentForm, email: e.target.value })
-              }
-              value={studentForm.email}
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              className="block w-full px-4 py-2 mt-2 text-white bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              onChange={(e) =>
-                setStudentForm({ ...studentForm, password: e.target.value })
-              }
-              value={studentForm.password}
-            />
-          </div>
-          <a href="#" className="text-xs text-white hover:underline">
-            Forget Password?
-          </a>
-          <div className="mt-6">
-<<<<<<< HEAD
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Login
-=======
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Sign up
->>>>>>> 423d3d3 (routes and auto search fix)
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+
+  <div className=" relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-gray-700 rounded-md shadow-xl lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-white underline">
+                   Student Sign up
+                </h1>
+                <form className="mt-6 text-black"
+                onSubmit={(e) => handleSubmitStudentForm(e)}
+                >
+                    <div className="mb-2">
+                        <label
+                            for="email"
+                            className="block text-sm font-semibold text-white"
+                        >
+                            Email
+                        </label>
+                        <input
+                            onChange={(e) => {setStudentForm({...studentForm, email: e.target.value})}}
+                            type="email"
+                            className=" block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            for="Name"
+                            className="block text-sm font-semibold text-white"
+                        >
+                            Name
+                        </label>
+                        <input
+                        onChange={(e) => {setStudentForm({...studentForm, name: e.target.value})}}
+                            type="text"
+                            className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            for="password"
+                            className="block text-sm font-semibold text-white"
+                        >
+                            Password
+                        </label>
+                        <input
+                        onChange={(e) => {setStudentForm({...studentForm, password: e.target.value})}}
+                            type="password"
+                            className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <a
+                        href="#"
+                        className="text-xs text-white hover:underline"
+                    >
+                        Forget Password?
+                    </a>
+                    <div className="mt-6">
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    // <form 
+    // onSubmit={(e) => handleSubmitStudentForm(e)}
+    // >
+    //   <input
+    //   onChange={(e) => setStudentForm({...studentForm, email: e.target.value})}
+    //   placeholder="EMAIL"
+    //   value={studentForm.email}
+    //   ></input>
+    //   <input 
+    //   onChange={(e) => setStudentForm({...studentForm, name: e.target.value})}
+    //   placeholder="name"
+    //   value={studentForm.name}
+    //   ></input>
+    //   <input placeholder="Password"
+    //   onChange={(e) => setStudentForm({...studentForm, password: e.target.value})}
+    //   value={studentForm.password}
+    //   ></input>
+    //   <button type="submit">Submit</button>
+    // </form>
+
   );
 }
